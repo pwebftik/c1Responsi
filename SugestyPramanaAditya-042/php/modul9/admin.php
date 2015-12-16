2b47036ccfe5384e197819e61a92cb710b3df2ff
sugestyprmndtya <sugestypramana27@gmail.com>
2015-12-16 08:46:07 +0800

    Modul 1,2,3,9

---
+++ SugestyPramanaAditya-042/php/modul9/admin.php
@@ -0,0 +1,30 @@
+<?php
+include 'koneksi.php';
+
+if(isset($_SESSION['login']) && $_SESSION['login']){
+	$user=$_SESSION['user'];
+	echo 'anda adalah user yang telah login dengan username '.$user['username'];
+	echo '<br> <a href="logout.php">LOGOUT</a>';
+}
+else {
+	echo 'pergi sana pencuri';
+}
+
+?>
+
+
+<?php if(isset($_SESSION['login']) && $_SESSION['login']) : ?>
+<?php $user = $_SESSION['user'];?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Panel Admin</title>
+</head>
+<body>
+<a href ="logout.php">logout</a>
+</body>
+</html>
+
+<?php else : ?>
+pergi sana
+<?php endif ?>
---
+++ SugestyPramanaAditya-042/php/modul9/sadmin.php
@@ -0,0 +1,5 @@
+<?php 
+include 'koneksi.php';
+ ?>
+ <?php if (isset($_SESSION['login']) && $_SESSION['login']) :?>
+ <?php if (isset($_SESSION['login']) && $_SESSION['login']) :?>
